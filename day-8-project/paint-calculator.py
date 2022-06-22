9fee1907c74cb2cdb94e1138ed514816e210fe50
WinTer1165 <86259597+WinTer1165@users.noreply.github.com>
2022-06-22 21:51:03 +0600

    Delete day-8-project directory

--- day-8-project/paint-calculator.py
+++
@@ -1,9 +0,0 @@
-def paint_calc(height, width, cover):
-    number_of_cans = round((height*width)/cover+0.5)
-    print(f"You'll need {number_of_cans} cans of paint.")
-
-
-test_h = int(input("Height of wall: "))
-test_w = int(input("Width of wall: "))
-coverage = 5
-paint_calc(height=test_h, width=test_w, cover=coverage)
