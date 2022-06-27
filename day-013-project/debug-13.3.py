ee2d6a6078e4c614b4d1e3ab85d39dc04dcabd48
WinTer1165 <86259597+WinTer1165@users.noreply.github.com>
2022-06-27 22:40:56 +0600

    Add files via upload

---
+++ day-013-project/debug-13.3.py
@@ -0,0 +1,22 @@
+# for number in range(1, 101):
+#   if number % 3 == 0 or number % 5 == 0:
+#     print("FizzBuzz")
+#   if number % 3 == 0:
+#     print("Fizz")
+#   if number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print([number])
+
+for number in range(1, 101):
+
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+
+    elif number % 3 == 0:
+        print("Fizz")
+
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
